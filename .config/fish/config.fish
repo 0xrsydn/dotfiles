8a2e735875b39124eec82f90addf1df7731762d6
0xrsydn <rafayrussani@gmail.com>
2026-02-27 05:28:26 +0700

    initial dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+alias dotfiles='git --git-dir=/home/rsydn/.dotfiles/ --work-tree=/home/rsydn'
---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+
+-- Basic keymaps
+vim.g.mapleader = ' '
